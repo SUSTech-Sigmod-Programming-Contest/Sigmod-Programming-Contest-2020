ea92ab921551b310e096930a34f7411e642db730
HollowKnight123 <50082146+HollowKnight123@users.noreply.github.com>
2020-02-16 03:48:28 +0800

    Add files via upload

--- baseline.py
+++ baseline.py
@@ -6,12 +6,21 @@ dataset_path = './page_title'
 output_path = './brandname'
 brandList = ['Nikon', 'Kodak', 'Sony', 'Canon', 'Go Pro', 'GoPro', 'Olympus', 'Fuji', 'Samsung', 'Panasonic', 'Leica',
              'Vivitar', 'Pentax', 'Hikvision', 'Polaroid', 'Minolta', 'Casio', 'Ricoh', 'BenQ', 'Dahua', 'Cannon',
-             'Toshiba', 'Minox', 'HP ', 'SVP', 'VistaQuest', 'Vista Quest', 'Vizio', 'JVC', 'Lexar','B+W']
+             'Toshiba', 'Minox', 'SVP', 'VistaQuest', 'Vista Quest', 'Vizio', 'JVC', 'Lexar', 'B+W',
+             'BELL+HOWELL', 'Bell Howell ', 'Coleman', 'DJI', 'Hoya', 'SanDisk', 'Sekonic', 'Lytro Light Field',
+             'Sanyo', 'Aquapix', 'Sigma SD', 'Aiptek',
+             'Philips', 'HASSELBLAD', 'CELESTRON', 'Sigma DP', 'Pioneer Elite', 'Konica', 'Cobra', 'Sealife', 'Argus',
+             'Skyworth', 'Seiki', 'Lowrance Elite','Kyocera','Staples Corrugated Shipping Boxes','coolpix']
+
+# merge-pair: Knoica - Minolta ; coolpix-nikon
+shortname_brandList = ['GE', 'LG', 'HP']
+accessories_brandList = ['Lowepro', 'Samyang']
+spelling_error = ['Fujufilm']
 columns_df = ['id', 'page_title']
 vis = {}
 
 
-def blocking():
+def blocking_in_general():
     for brand in brandList:
         data = {'id': [], 'page_title': []}
         for website in os.listdir(dataset_path):
@@ -35,6 +44,33 @@ def blocking():
         df.to_csv(output_path + '/' + brand + '.csv', index=False)
         print(brand)
 
+
+def blocking_in_shortname():
+    for brand in shortname_brandList:
+        data = {'id': [], 'page_title': []}
+        for website in os.listdir(dataset_path):
+            with open(dataset_path + '/' + website, 'r', encoding='UTF-8') as file:
+                reader = csv.reader(file)
+                i = False
+                for row in reader:
+                    if i:
+                        num = row[0]
+                        key = website[0:-4] + '//' + num
+                        page_title = row[1]
+                        words = page_title.split(' ')
+                        for word in words:
+                            if word == brand:
+                                data['id'].append(key)
+                                data['page_title'].append(page_title)
+                                vis[key] += 1
+                    else:
+                        i = True
+        df = pd.DataFrame(data, columns=columns_df)
+        df.to_csv(output_path + '/' + brand + '.csv', index=False)
+        print(brand)
+
+
+def collecting_remain():
     data = {'id': [], 'page_title': []}
     for website in os.listdir(dataset_path):
         with open(dataset_path + '/' + website, 'r', encoding='UTF-8') as file:
@@ -52,6 +88,12 @@ def blocking():
                     i = True
     df = pd.DataFrame(data, columns=columns_df)
     df.to_csv(output_path + '/' + 'others' + '.csv', index=False)
+
+
+def blocking():
+    blocking_in_general()
+    blocking_in_shortname()
+    collecting_remain()
 
 
 blocking()
