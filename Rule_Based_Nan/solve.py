e8bb986dede660866966523f78c6539fedf8ba5f
ludwig <ludwig98t@gmail.com>
2020-03-18 17:43:42 +0800

    update solve

--- Rule_Based_Nan/solve.py
+++ Rule_Based_Nan/solve.py
@@ -12,9 +12,11 @@ list = {'Fujifilm': 1, 'Samsung': 1, 'Pentax': 1, 'Panasonic': 1, 'Olympus': 1,'
 
 def solve():
     for source in tqdm(os.listdir(DATA_PATH)):
+        if source == '.DS_Store':
+            continue
         for model in os.listdir(os.path.join(DATA_PATH, source)):
             if model == 'others.csv':
-                os.remove(DATA_PATH+'/'+source+'/'+model)
+                # os.remove(DATA_PATH+'/'+source+'/'+model)
                 continue
             with open(DATA_PATH + '/' + source + '/' + model, 'r', encoding='UTF-8') as file:
                 reader = csv.reader(file)
